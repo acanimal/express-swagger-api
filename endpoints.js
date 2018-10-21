91908990918049d4bf60b54898bd89a128ac1bb5
acanimal <asantiagop@gmail.com>
2018-10-21 13:48:26 +0200

    Initial import

---
+++ endpoints.js
@@ -0,0 +1,13 @@
+const initializeEndpoints = (app) => {
+  /** 
+   * @swagger
+   * /users:
+   *    get:
+   *      description: This should return all users
+   */
+  app.get('/users', (req, res) => res.end('This sould return all users'));
+
+  app.get('/users/:id', (req, res) => res.end(`This should return the user with ID ${req.params.id}`));
+}
+
+module.exports = initializeEndpoints;
